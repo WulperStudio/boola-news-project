2ce3bda3fc0e51cebf1e87b1ec2b39c1fd0f1e7f
Angel Silva <angel.silva@avantrip.com>
2020-11-13 15:11:21 -0300

    Initial Config: Gastby, Storybook, Theme Material & Scaffold Atomic Design

---
+++ src/utils/themes.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const defaultTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#7155F6',
+    }
+  },
+});
+
+export const darkTheme = createMuiTheme({
+});
