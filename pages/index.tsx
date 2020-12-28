71b9dfa788615e28fd3d5d4222c6314e0cd4bc05
angelarsd <angel.arsd@gmail.com>
2020-12-28 19:53:06 -0300

    version 0.0.1-beta-4 @wulpers-ui/core

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Blog from "@wulpers-ui/core/components/templates/Blog"
 import CardBlog from "@wulpers-ui/core/components/molecules/CardBlog/CardBlog"
 import { useRouter } from "next/router"
 import { NextApiRequest } from "next"
+import Grid from "@material-ui/core/Grid"
 
 const Home = ({ data }: any) => {
   const router = useRouter()
@@ -13,28 +14,96 @@ const Home = ({ data }: any) => {
         <title>{data.title}</title>
         <meta name="description" content={data.description} />
       </Head>
+
       <br />
-      {data.posts &&
-      data.posts.map(post => (
-        <CardBlog
-          key={post.title}
-          variant="type2"
-          data={{
-            title: post.title,
-            preTitle: "preTitle",
-            content: post.content,
-            image: process.env.strapiServer + post.image[0].url
-          }}
-          onClick={() => {
-            router.push(`/[slug]`, `/${post.slug}`, { shallow: true })
-          }}
-        />
-      ))}
+
+      {data.posts[0] && (
+        <>
+          <CardBlog
+            key={data.posts[0].title}
+            variant="type2"
+            data={{
+              title: data.posts.title,
+              preTitle: "preTitle",
+              content: data.posts[0].content,
+              image: data.posts[0].image[0] ? process.env.strapiServer + data.posts[0].image[0].url : "https://boola-news-admin.herokuapp.com/uploads/card_image_b9274b39f7.png"
+            }}
+            onClick={() => {
+              router.push(`/[slug]`, `/${data.posts[0].slug}`, { shallow: true })
+            }}
+          />
+          <br />
+        </>
+      )}
+      <Grid container spacing={3}>
+        {data.posts[1] && (
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <CardBlog
+              key={data.posts[1].title}
+              variant="type1"
+              data={{
+                title: data.posts.title,
+                preTitle: "preTitle",
+                content: data.posts[1].content,
+                image: data.posts[1].image[0] ? process.env.strapiServer + data.posts[1].image[0].url : "https://boola-news-admin.herokuapp.com/uploads/card_image_b9274b39f7.png"
+              }}
+              onClick={() => {
+                router.push(`/[slug]`, `/${data.posts[1].slug}`, { shallow: true })
+              }}
+            />
+          </Grid>
+        )}
+
+        {data.posts[2] && (
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <CardBlog
+              key={data.posts[2].title}
+              variant="type1"
+              data={{
+                title: data.posts.title,
+                preTitle: "preTitle",
+                content: data.posts[2].content,
+                image: data.posts[2].image[0] ? process.env.strapiServer + data.posts[2].image[0].url : "https://boola-news-admin.herokuapp.com/uploads/card_image_b9274b39f7.png"
+              }}
+              onClick={() => {
+                router.push(`/[slug]`, `/${data.posts[2].slug}`, { shallow: true })
+              }}
+            />
+          </Grid>
+        )}
+
+        {data.posts[3] && (
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <CardBlog
+              key={data.posts[3].title}
+              variant="type1"
+              data={{
+                title: data.posts.title,
+                preTitle: "preTitle",
+                content: data.posts[3].content,
+                image: data.posts[3].image[0] ? process.env.strapiServer + data.posts[3].image[0].url : "https://boola-news-admin.herokuapp.com/uploads/card_image_b9274b39f7.png"
+              }}
+              onClick={() => {
+                router.push(`/[slug]`, `/${data.posts[3].slug}`, { shallow: true })
+              }}
+            />
+          </Grid>
+        )}
+      </Grid>
+
     </Blog>
   )
 }
 
-export const getServerSideProps = async ({ req }: { req: NextApiRequest }) => {
+export const getServerSideProps = async (
+  {
+    req
+  }
+    :
+    {
+      req: NextApiRequest
+    }
+) => {
   const queryGrpahQL = `query {
     blogs(where: { domain: "${req.headers.host}" }) {
       title
