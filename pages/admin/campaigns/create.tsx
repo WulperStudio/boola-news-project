85f0623a6ce65e7bf41dc46d00ca906fc958a665
angelarsd <angel.arsd@gmail.com>
2021-01-18 19:35:07 -0300

    @wulpers-ui/core@0.0.1-beta-10

--- pages/admin/campaigns/create.tsx
+++ pages/admin/campaigns/create.tsx
@@ -3,17 +3,17 @@ import Router from "next/router"
 import axios from "axios"
 import AdminTheme from "@wulpers-ui/core/components/templates/Admin"
 import AsideFixed from "@wulpers-ui/core/components/containers/AsideFixed/AsideFixed"
-import Menu from "@wulpers-ui/core/components/molecules/Menu/Menu"
 import TextField from "@wulpers-ui/core/components/atoms/Form/TextField"
 import Dropzone from "@wulpers-ui/core/components/atoms/DropZone"
-import Fab from "@wulpers-ui/core/components/atoms/Fab"
 import FormLabel from "@wulpers-ui/core/components/atoms/FormLabel"
 import Typography from "@wulpers-ui/core/components/atoms/Typography"
 import Switch from "@wulpers-ui/core/components/atoms/Switch"
-import FilterIcon from "@wulpers-ui/core/components/icons/Filter"
+import CustomFormPost, {ValidateForm} from "@wulpers-ui/core/components/organisms/CustomFormPost"
+import FormRow, {
+  FormContainer,
+} from "@wulpers-ui/core/components/containers/FormRow"
 import { getSessionData } from "../../../utils/middleware"
-
-const slug = require("slug")
+import slug from "slug"
 
 export default function Create({ token, domain, dataSession, dataBlog }: any) {
   console.log("dataSession>>>", JSON.stringify(dataSession))
@@ -30,7 +30,8 @@ export default function Create({ token, domain, dataSession, dataBlog }: any) {
     winned: 0,
     image: [],
     blog: dataBlog,
-    responsable: dataSession
+    responsable: dataSession,
+    customForm: []
   })
   const [images, setImages] = useState([])
   const [loading, setLoading] = useState(true)
@@ -39,13 +40,25 @@ export default function Create({ token, domain, dataSession, dataBlog }: any) {
   const [imageError, setImageError] = useState(false)
   const [contentError, setContentError] = useState(false)
 
+  const setCustomForm=(customForm) =>{
+    setData({...data,customForm})
+  }
+
   useEffect(() => {
     setLoading(false)
   }, [])
 
+  useEffect(() => {
+    console.log("images>>>",images)
+  }, [images])
 
   function createPost(preview: boolean) {
-    if (!data.title || !data.slug || !images.length || !data.content) {
+    const validateCustomForm = ValidateForm(data.customForm)
+    //console.log("validateCustomForm>>>",validateCustomForm)
+    if (validateCustomForm.errors){
+      setCustomForm(validateCustomForm.values)
+    }
+    /*if (!data.title || !data.slug || !images.length || !data.content) {
       setTitleError(!data.title)
       setSlugError(!data.slug)
       setImageError(!images.length)
@@ -58,14 +71,22 @@ export default function Create({ token, domain, dataSession, dataBlog }: any) {
       })
       return axios
         .post(`${process.env.strapiServer}/upload`, formData, {
-          headers: { "Content-Type": "multipart/form-data", "Authorization": `Bearer ${token}` }
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
         })
         .then(res => {
-          return axios.post(`${process.env.strapiServer}/posts`, { ...data, image: res.data }, {
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
-          })
+          return axios
+            .post(
+              `${process.env.strapiServer}/posts`,
+              { ...data, image: res.data },
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            )
             .then(response => {
               console.log("Data: ", response.data)
               if (preview) {
@@ -82,7 +103,7 @@ export default function Create({ token, domain, dataSession, dataBlog }: any) {
           console.log(err)
           return err
         })
-    }
+    }*/
   }
 
   return (
@@ -96,109 +117,115 @@ export default function Create({ token, domain, dataSession, dataBlog }: any) {
         {
           title: "Preview",
           onClick: () => createPost(true),
-          type: "Button"
+          type: "Button",
         },
         {
           title: "Create",
           onClick: () => createPost(false),
-          type: "Button"
-        }]}
+          type: "Button",
+        },
+      ]}
       loading={loading}
     >
-      <AsideFixed asideContent={
-        <Menu
-          button={
-            <Fab
-              color="inherit"
-              size="medium"
-              aria-label="edit"
-            >
-              <FilterIcon />
-            </Fab>
-          }
-          items={["Insert image", "Insert video", "Insert phrase", "Insert URL", "Insert subtitle"]}
+      <AsideFixed>
+        <FormContainer>
+          
+          <FormRow>
+            <Typography gutterBottom variant="h6" component="h2" color="primary">
+              {"POST DESCRIPTION -> Title section"}
+            </Typography>
+          </FormRow>
+
+          <FormRow>
+            <TextField
+              label="Title"
+              error={titleError}
+              helperText={titleError ? "Incorrect entry." : ""}
+              fullWidth
+              defaultValue={data.title}
+              value={data.title}
+              onChange={(e: any) => {
+                setTitleError(false)
+                setSlugError(false)
+                setData({
+                  ...data,
+                  title: e.target.value,
+                  slug: slug(e.target.value),
+                })
+              }}
+            />
+          </FormRow>
+
+          <FormRow>
+            <TextField
+              label="Slug"
+              error={slugError}
+              helperText={slugError ? "Incorrect entry." : ""}
+              fullWidth
+              defaultValue={data.slug}
+              value={data.slug}
+              InputProps={{ readOnly: true }}
+              onChange={(e: any) => {
+                setSlugError(false)
+                setData({ ...data, slug: e.target.value })
+              }}
+            />
+          </FormRow>
+
+          <FormRow>
+            <Dropzone
+              label="Attach image"
+              error={imageError}
+              helperText={imageError ? "Incorrect entry." : ""}
+              onChange={(files: any) => {
+                setImageError(false)
+                setImages(files)
+              }}
+            />
+          </FormRow>
+
+          <FormRow>
+            <FormLabel size="small" component="legend">
+              Status
+            </FormLabel>
+            <Switch
+              checked={data.status === "Publish"}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setData({
+                  ...data,
+                  status: e.target.checked ? "Publish" : "Draft",
+                  publishedDate: e.target.checked ? Date.now().toString() : "",
+                })
+              }
+              name="checkedA"
+              inputProps={{ "aria-label": "secondary checkbox" }}
+            />
+            {data.status}
+          </FormRow>
+
+          <FormRow>
+            <TextField
+              label="Paragraph"
+              error={contentError}
+              helperText={contentError ? "Incorrect entry." : ""}
+              fullWidth
+              multiline
+              rows={4}
+              defaultValue={data.content}
+              value={data.content}
+              onChange={e => {
+                setContentError(false)
+                setData({ ...data, content: e.target.value })
+              }}
+            />
+          </FormRow>
+          
+        </FormContainer>
+        <CustomFormPost
+          title = "CONTENT -> Title section"
+          values={data.customForm}
+          setValues={setCustomForm}
         />
-      }>
-        <Typography gutterBottom variant="h6" component="h2" color="primary">
-          POST DESCRIPTION -> Title section
-        </Typography>
-
-        <div style={{ padding: "12px 0" }}>
-          <TextField
-            label="Title"
-            error={titleError}
-            helperText={titleError ? "Incorrect entry." : ""}
-            fullWidth
-            defaultValue={data.title} value={data.title}
-            onChange={(e: any) => {
-              setTitleError(false)
-              setSlugError(false)
-              setData({ ...data, title: e.target.value, slug: slug(e.target.value) })
-            }}
-          />
-        </div>
-
-        <div style={{ padding: "12px 0" }}>
-          <TextField
-            label="Slug"
-            error={slugError}
-            helperText={slugError ? "Incorrect entry." : ""}
-            fullWidth
-            defaultValue={data.slug}
-            value={data.slug}
-            InputProps={{ readOnly: true }}
-            onChange={(e: any) => {
-              setSlugError(false)
-              setData({ ...data, slug: e.target.value })
-            }} />
-        </div>
-
-        <div style={{ padding: "12px 0" }}>
-          <FormLabel error={imageError} size="small" component="legend">Attach image</FormLabel>
-          {<Dropzone
-            error={imageError}
-            helperText={imageError ? "Incorrect entry." : ""}
-            onChange={(files: any) => {
-              setImageError(false)
-              setImages(files)
-            }}
-          />}
-        </div>
-
-        <div style={{ padding: "12px 0" }}>
-          <FormLabel size="small" component="legend">Status</FormLabel>
-          <Switch
-            checked={data.status === "Publish"}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setData({
-              ...data,
-              status: e.target.checked ? "Publish" : "Draft",
-              publishedDate: e.target.checked ? Date.now().toString() : ""
-            })}
-            name="checkedA"
-            inputProps={{ "aria-label": "secondary checkbox" }}
-          />{data.status}
-        </div>
-
-        <div style={{ padding: "12px 0" }}>
-          <TextField
-            label="Paragraph"
-            error={contentError}
-            helperText={contentError ? "Incorrect entry." : ""}
-            fullWidth multiline rows={4}
-            defaultValue={data.content}
-            value={data.content}
-            onChange={e => {
-              setContentError(false)
-              setData({ ...data, content: e.target.value })
-            }} />
-        </div>
-
-        {/*<br />
-          <Typography gutterBottom variant="h6" component="h2" color="primary">
-          CONTENT -> Title section
-          </Typography>*/}
-
-
       </AsideFixed>
     </AdminTheme>
   )
